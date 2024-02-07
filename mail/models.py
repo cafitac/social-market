054787876fab160685cf8bca4b6e33279947e0b6
cafitac <cafitac99@gmail.com>
2024-02-08 04:39:42 +0900

    feat: ActivateMail 모델 생성

--- mail/models.py
+++ mail/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 
+from utils.model.base import AbstractBaseModel
 
-class MailHistory(models.Model):
+
+class ActivateMail(AbstractBaseModel):
+    """
+    사용자에 대한 인증 메일의 활성화 상태를 관리
+    """
+
+    user_id = models.BigIntegerField(null=False, unique=True)
+    activate_code = models.CharField(max_length=128, null=False, unique=True)
+    is_activated = models.BooleanField(default=False)
 
     class Meta:
-        db_table = "mail_history"
-        verbose_name = "Mail History"
+        db_table = "activate_mail"
+        verbose_name = "Activate Mail"
         verbose_name_plural = f"{verbose_name} List"
