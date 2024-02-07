a798127ac094ba2861cc1f1b9931ca3714d3cf45
cafitac <cafitac99@gmail.com>
2024-02-08 05:21:29 +0900

    test: 회원가입에 대한 테스트 코드 작성

--- member/tests.py
+++ member/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
 
-# Create your tests here.
+from member.models import User
+
+
+class UserViewTestCase(TestCase):
+
+    def test_사용자가_회원가입을_합니다(self) -> None:
+        res = self.client.post(
+            path="/api/member/users",
+            data={
+                "username": "user1",
+                "password": "user1user1",
+                "re_password": "user1user1",
+                "email": "user1@gmail.com",
+            },
+            content_type="application/json",
+        )
+        self.assertEquals(res.status_code, 201)
+
+        data: dict = res.json()
+        self.assertTrue(User.objects.filter(username=data['username']).exists())
