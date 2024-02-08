0fe5cff0c3c88251987a7bc2176ea9900ff660a5
cafitac <cafitac99@gmail.com>
2024-02-08 12:14:17 +0900

    test: 사용자가 메일 인증을 통해 계정을 활성화하는 테스트 케이스 작성

--- mail/tests.py
+++ mail/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
 
-# Create your tests here.
+from utils.test.fixtures import TestFixture
+
+
+class MailViewTestCase(TestCase):
+
+    def setUp(self):
+        self.비활성화_사용자 = TestFixture().non_active_user
+        self.비활성화_사용자.save()
+
+    def test_사용자가_메일_인증을_통해_계정을_활성화합니다(self):
+        self.client.force_login(self.비활성화_사용자)
+
+        user_active_mail = TestFixture().active_mail
+        user_active_mail.save()
+
+        res = self.client.get(path=f"/api/mail/{user_active_mail.activate_code}")
+        self.assertEquals(res.status_code, 200)
+
+        user_active_mail.refresh_from_db()
+
+        self.assertTrue(user_active_mail.is_expired)
