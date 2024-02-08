49ff184732f79fe00b789236f219907cb8201c9d
cafitac <cafitac99@gmail.com>
2024-02-08 14:40:50 +0900

    feat: 메일 인증 후, 만료 처리하는 서비스 코드 작성

---
+++ domain/mail/services/mail_command_service.py
@@ -0,0 +1,15 @@
+from rest_framework.exceptions import ValidationError
+
+from domain.mail.models import UserActiveMail
+
+
+class MailCommandService:
+
+    @staticmethod
+    def check_user_active_mail_is_valid_and_expired(user_active_mail: str) -> None:
+        try:
+            user_active_mail: UserActiveMail = UserActiveMail.objects.get(active_code=user_active_mail)
+            user_active_mail.expired()
+            user_active_mail.save()
+        except UserActiveMail.DoesNotExist:
+            raise ValidationError("인증 코드가 일치하지 않습니다.")
