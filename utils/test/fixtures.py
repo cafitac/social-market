39f5ded8dd4b37bc338c224e0c0ae6d3e4f183ec
cafitac <cafitac99@gmail.com>
2024-02-08 12:13:51 +0900

    test: 재활용을 위해 반복 사용되는 객체 Fixture 로 등록

---
+++ utils/test/fixtures.py
@@ -0,0 +1,13 @@
+from mail.models import UserActiveMail
+from member.models import User
+
+
+class TestFixture:
+
+    def __init__(self):
+        # Member
+        self.non_active_user = User(id=1, username='user1', password='user1user1', is_active=False)
+        self.active_user = User(id=2, username='user1', password='user1user1', is_active=True)
+
+        # Mail
+        self.active_mail = UserActiveMail(id=1, user_id=self.non_active_user.id, activate_code="active_code")
