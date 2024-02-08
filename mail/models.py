3fc0290c0996408b670da11a40bbacc3f2a970b8
cafitac <cafitac99@gmail.com>
2024-02-08 12:07:07 +0900

    chore: 유저 활성화 메일에 대한 테이블 이름 변경

--- mail/models.py
+++ mail/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from utils.model.base import AbstractBaseModel
 
 
-class ActivateMail(AbstractBaseModel):
+class UserActiveMail(AbstractBaseModel):
     """
     사용자에 대한 인증 메일의 활성화 상태를 관리
     """
@@ -13,6 +13,9 @@ class ActivateMail(AbstractBaseModel):
     is_expired = models.BooleanField(default=False)
 
     class Meta:
-        db_table = "activate_mail"
-        verbose_name = "Activate Mail"
+        db_table = "user_active_mail"
+        verbose_name = "User Active Mail"
         verbose_name_plural = f"{verbose_name} List"
+
+    def expired(self):
+        self.is_expired = True
