fe924aa5f7276a2725894202e08af47a1f683b61
cafitac <cafitac99@gmail.com>
2024-02-09 19:46:14 +0900

    test: 상품 등록에 대한 테스트 코드 작성

--- merchandise/tests.py
+++ merchandise/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
 
-# Create your tests here.
+from member.models import User
+from merchandise.models import Merchandise
+from utils.test.fixtures import TestFixture
+
+
+class MerchandiseViewTestCase(TestCase):
+
+    def setUp(self):
+        self.사용자: User = TestFixture().active_user
+        self.사용자.save()
+
+    def test_create_merchandise(self):
+        self.client.force_login(self.사용자)
+
+        res = self.client.post(
+            path="/api/merchandise",
+            data={
+                "name": "상품",
+                "description": "상품 설명",
+                "price": 10000,
+            },
+            content_type="application/json",
+        )
+        self.assertEquals(res.status_code, 201)
+
+        data = res.json()
+
+        self.assertTrue(Merchandise.objects.filter(pk=data['id']).exists())
