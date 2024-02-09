28a3049aef67245e542b55fc87a6b5984cd08c06
cafitac <cafitac99@gmail.com>
2024-02-10 02:06:28 +0900

    feat: 정적 팩토리 메서드 작성 및 user_id 필드 추가

--- merchandise/models/merchandise.py
+++ merchandise/models/merchandise.py
@@ -4,6 +4,7 @@ from utils.model.base import AbstractBaseModel
 
 
 class Merchandise(AbstractBaseModel):
+    user_id = models.IntegerField(null=False)
     name = models.CharField(max_length=50, blank=False, null=False)
     description = models.TextField(blank=True, null=False)
     price = models.IntegerField(null=False)
@@ -12,3 +13,7 @@ class Merchandise(AbstractBaseModel):
         db_table = 'merchandise'
         verbose_name = "Merchandise"
         verbose_name_plural = f"{verbose_name} List"
+
+    @staticmethod
+    def create(user_id: int, name: str, description: str, price: int) -> 'Merchandise':
+        return Merchandise(user_id=user_id, name=name, description=description, price=price)
