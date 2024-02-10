be2dfe5017fa8983b29a9986f824b701a7836a9b
cafitac <cafitac99@gmail.com>
2024-02-11 07:06:24 +0900

    test: 상품 상세 정보 수정에 대한 권한 테스트 코드를 작성합니다

--- merchandise/tests.py
+++ merchandise/tests.py
@@ -10,8 +10,10 @@ class MerchandiseViewTestCase(TestCase):
     def setUp(self):
         self.사용자: User = TestFixture().active_user
         self.사용자.save()
+        self.기타_사용자: User = TestFixture().other_user
+        self.기타_사용자.save()
 
-    def test_create_merchandise(self) -> Merchandise:
+    def test_사용자가_상품을_등록할_수_있다(self) -> Merchandise:
         self.client.force_login(self.사용자)
 
         res = self.client.post(
@@ -31,8 +33,8 @@ class MerchandiseViewTestCase(TestCase):
 
         return Merchandise.objects.get(pk=data['id'])
 
-    def test_get_own_merchandises(self):
-        self.test_create_merchandise()
+    def test_사용자가_자신이_등록한_상품_목록을_조회할_수_있다(self):
+        self.test_사용자가_상품을_등록할_수_있다()
 
         res = self.client.get(path="/api/merchandise/merchandises")
         self.assertEquals(res.status_code, 200)
@@ -41,8 +43,8 @@ class MerchandiseViewTestCase(TestCase):
 
         self.assertEquals(len(data), 1)
 
-    def test_get_own_merchandise(self):
-        merchandise: Merchandise = self.test_create_merchandise()
+    def test_사용자가_자신이_등록한_상품_상세_정보를_조회할_수_있다(self):
+        merchandise: Merchandise = self.test_사용자가_상품을_등록할_수_있다()
 
         res = self.client.get(path=f"/api/merchandise/merchandises/{merchandise.id}")
         self.assertEquals(res.status_code, 200)
@@ -51,8 +53,8 @@ class MerchandiseViewTestCase(TestCase):
 
         self.assertEquals(data['name'], merchandise.name)
 
-    def test_update_own_merchandise(self):
-        merchandise: Merchandise = self.test_create_merchandise()
+    def test_사용자가_자신이_등록한_상품의_정보를_수정할_수_있다(self):
+        merchandise: Merchandise = self.test_사용자가_상품을_등록할_수_있다()
 
         res = self.client.patch(
             path=f"/api/merchandise/merchandises/{merchandise.id}",
@@ -69,3 +71,18 @@ class MerchandiseViewTestCase(TestCase):
         merchandise.refresh_from_db()
 
         self.assertEquals(merchandise.name, "수정된 상품 이름")
+
+    def test_기타_사용자는_사용자가_등록한_상품의_정보를_수정할_수_없다(self):
+        merchandise: Merchandise = self.test_사용자가_상품을_등록할_수_있다()
+        self.client.force_login(self.기타_사용자)
+
+        res = self.client.patch(
+            path=f"/api/merchandise/merchandises/{merchandise.id}",
+            data={
+                "name": "수정된 상품 이름",
+                "description": "수정된 상품 설명",
+                "price": 5000,
+            },
+            content_type="application/json",
+        )
+        self.assertEquals(res.status_code, 403)
--- utils/test/fixtures.py
+++ utils/test/fixtures.py
@@ -7,7 +7,8 @@ class TestFixture:
     def __init__(self):
         # Member
         self.non_active_user = User(id=1, username='user1', password='user1user1', is_active=False)
-        self.active_user = User(id=2, username='user1', password='user1user1', is_active=True)
+        self.active_user = User(id=2, username='active_user', password='active_useractive_user', is_active=True)
+        self.other_user = User(id=3, username='other_user', password='other_userother_user', is_active=True)
 
         # Mail
         self.active_mail = UserActiveMail(id=1, user_id=self.non_active_user.id, active_code="active_code")
