c380c8d880d031b258852731e18cfc68d7471442
cafitac <cafitac99@gmail.com>
2024-02-11 05:28:35 +0900

    feat: 상품에 대한 어드민 기능 등록

--- merchandise/admin.py
+++ merchandise/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from merchandise.models import Merchandise
+
+
+@admin.register(Merchandise)
+class MerchandiseAdmin(admin.ModelAdmin):
+    pass
