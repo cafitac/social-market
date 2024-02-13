d010be485b006bb0eebfc2ca3a7abc883d5098e3
cafitac <cafitac99@gmail.com>
2024-02-13 22:34:03 +0900

    fix: csrf 문제를 해결합니다

--- static/js/merchandise/delete.js
+++ static/js/merchandise/delete.js
@@ -1,6 +1,10 @@
 function deleteMerchandise(merchandiseId) {
+    csrftoken = getCookie('csrftoken')
     fetch(`http://localhost:8000/api/merchandise/merchandises/${merchandiseId}`, {
         method: "DELETE",
+        headers: {
+            "X-CSRFToken": csrftoken,
+        },
     })
         .then((response) => {
             if (response.status === 204) {
@@ -15,5 +19,21 @@ $(".deleteButton").on('click', function () {
 });
 
 $("#deleteConfirm").on("click", function () {
-    deleteMerchandise($("#delete-merchandise-id").value)
+    deleteMerchandise($("#delete-merchandise-id").val())
 });
+
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
