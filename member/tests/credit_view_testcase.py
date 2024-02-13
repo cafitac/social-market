d7ac87eae47d2423bbfb2d4d62f103402d47cb27
cafitac <cafitac99@gmail.com>
2024-02-13 12:24:57 +0900

    test: 크레딧과 관련된 테스트 케이스를 작성합니다

---
+++ member/tests/credit_view_testcase.py
@@ -0,0 +1,39 @@
+from django.test import TestCase
+
+from member.models import Credit, User
+from utils.test.fixtures import TestFixture
+
+
+class CreditViewTestCase(TestCase):
+
+    def setUp(self):
+        fixture = TestFixture()
+        self.사용자: User = fixture.active_user
+        self.사용자.save()
+        self.크레딧: Credit = fixture.credit
+        self.크레딧.save()
+
+    def test_사용자는_크레딧을_조회할_수_있다(self):
+        # when
+        res = self.client.get(path="/api/member/members/credit")
+
+        # then
+        self.assertEquals(res.status_code, 200)
+
+        data = res.json()
+        self.assertEquals(data['balance'], 0)
+
+    def test_사용자는_결제에_필요한_크레딧을_충전할_수_있다(self):
+        # when
+        res = self.client.post(
+            path="/api/member/members/credit",
+            data={
+                "balance": 10000,
+            },
+            content_type="application/json",
+        )
+
+        # then
+        self.assertEquals(res.status_code, 200)
+        data = res.json()
+        self.assertEquals(data['balance'], 10000)
