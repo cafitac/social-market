d1e4a9b3b88e2d2703772af9a4d35a8d68b897c5
cafitac <cafitac99@gmail.com>
2024-02-13 12:44:11 +0900

    test: Fixture 에 크레딧 객체를 추가합니다

--- utils/test/fixtures.py
+++ utils/test/fixtures.py
@@ -1,5 +1,6 @@
 from mail.models import UserActiveMail
 from member.models import User
+from member.models.credit import Credit
 
 
 class TestFixture:
@@ -9,6 +10,9 @@ class TestFixture:
         self.non_active_user = User(id=1, username='user1', password='user1user1', is_active=False)
         self.active_user = User(id=2, username='active_user', password='active_useractive_user', is_active=True)
         self.other_user = User(id=3, username='other_user', password='other_userother_user', is_active=True)
+
+        # Credit
+        self.credit = Credit(user=self.active_user)
 
         # Mail
         self.active_mail = UserActiveMail(id=1, user_id=self.non_active_user.id, active_code="active_code")
