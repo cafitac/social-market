1c672464402d79beda7affe92ed60f3ccd136d14
cafitac <cafitac99@gmail.com>
2024-02-13 12:24:43 +0900

    feat: Credit 클래스를 정의합니다

---
+++ member/models/credit.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+from member.models import User
+
+
+class Credit(models.Model):
+    user = models.OneToOneField(to=User, on_delete=models.CASCADE, null=False)
+    balance = models.IntegerField(default=0)
+
+    class Meta:
+        db_table = 'credit'
+        verbose_name = 'Credit'
+        verbose_name_plural = f'{verbose_name} List'
