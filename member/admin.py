d8dee231ddc20c937e987845292d7ebd9b856f63
cafitac <cafitac99@gmail.com>
2024-02-14 09:47:33 +0900

    feat: Credit 에 대한 관리자 기능을 추가합니다

--- member/admin.py
+++ member/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
 
-from member.models import User
+from member.models import Credit, User
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     fields = ("username", "email", "last_name", "first_name", "is_active", )
     readonly_fields = ("username", "email", "last_name", "first_name", )
+
+
+@admin.register(Credit)
+class CreditAdmin(admin.ModelAdmin):
+    list_display = ("user__username", "balance", )
+    fields = ("user", "balance", )
+    readonly_fields = ("user", )
+
+    def user__username(self, obj):
+        return obj.user.username
+    user__username.short_description = 'Username'
