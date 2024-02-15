36cc1d45d8571a9267d15e47a5cf348558871379
cafitac <cafitac99@gmail.com>
2024-02-15 11:57:41 +0900

    feat: Cart app등록

---
+++ cart/models/cart.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+from utils.model.base import AbstractBaseModel
+
+
+class Cart(AbstractBaseModel):
+    merchandise_id = models.IntegerField(null=False)
+    merchandise_name = models.CharField(max_length=150, blank=False, null=False)
+    merchandise_price = models.IntegerField(null=False)
+    merchandise_is_deleted = models.BooleanField(default=False)
+    is_deleted = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'cart'
+        verbose_name = "Cart"
+        verbose_name_plural = f"{verbose_name} List"
---
+++ cart/tests.py
@@ -0,0 +1,69 @@
+from django.test import TestCase
+
+from cart.models.cart import Cart
+from member.models import User
+from merchandise.models import Merchandise
+from utils.test.fixtures import TestFixture
+
+
+class CartViewTestCase(TestCase):
+    
+    def setUp(self):
+        fixture = TestFixture()
+        self.사용자: User = fixture.active_user
+        self.사용자.save()
+        self.상품: Merchandise = fixture.merchandise
+        self.상품.save()
+
+    def test_사용자가_상품을_장바구니에_추가할_수_있다(self):
+        # when
+        res = self.client.post(
+            path="/api/cart/carts/",
+            data={
+                "merchandise_id": self.상품.id,
+            },
+            content_type="application/json",
+        )
+
+        # then
+        self.assertEquals(res.status_code, 201)
+
+        data = res.json()
+        self.assertEquals(data['merchandise_id'], self.상품.id)
+
+    def test_사용자가_장바구니의_상품들을_조회할_수_있다(self):
+        # given
+        self._사용자가_장바구니에_상품을_추가함(self.상품.id)
+        
+        # when
+    
+        # then
+
+    def test_사용자가_장바구니에_있는_상품의_수를_수정할_수_있다(self):
+        pass
+        # given
+        
+        # when
+    
+        # then
+    
+    def test_사용자가_장바구니에_있는_상품을_제거할_수_있다(self):
+        pass
+        # given
+        
+        # when
+    
+        # then
+
+    def _사용자가_장바구니에_상품을_추가함(self, merchandise_id: int) -> Cart:
+        res = self.client.post(
+            path="/api/cart/carts/",
+            data={
+                "merchandise_id": merchandise_id,
+            },
+            content_type="application/json",
+        )
+        self.assertEquals(res.status_code, 201)
+
+        data = res.json()
+        return Cart.objects.get(pk=data['id'])
