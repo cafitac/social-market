9e34a2003b289ed06d82ef384dfa565c60247a71
cafitac <cafitac99@gmail.com>
2024-02-16 19:31:04 +0900

    test: 사용자가 주문을 생성하는 테스트 코드를 작성합니다

---
+++ order/tests.py
@@ -0,0 +1,57 @@
+from django.test import TestCase
+
+from member.models import User
+from merchandise.models import Merchandise
+from order.models.order import Order
+from utils.test.fixtures import TestFixture
+
+
+class OrderViewTestCase(TestCase):
+
+    def setUp(self):
+        testFixture: TestFixture = TestFixture()
+        self.사용자: User = testFixture.active_user
+        self.사용자.save()
+        self.상품_1: Merchandise = testFixture.merchandise_1
+        self.상품_1.save()
+        self.상품_2: Merchandise = testFixture.merchandise_2
+        self.상품_2.save()
+
+        self.client.force_login(self.사용자)
+
+    def test_사용자가_상품에_대한_주문을_생성할_수_있다(self):
+        # when
+        res = self.client.post(
+            path="/api/order/orders",
+            data={
+                "email": self.사용자.email,
+                "address": "서울시",
+                "order_items": [
+                    {
+                        "merchandise_id": self.상품_1.id,
+                        "amount": 1,
+                    },
+                    {
+                        "merchandise_id": self.상품_2.id,
+                        "amount": 2,
+                    }
+                ]
+            },
+            content_type="application/json",
+        )
+    
+        # then
+        self.assertEquals(res.status_code, 201)
+
+        data = res.json()
+
+        self.assertTrue(Order.objects.filter(pk=data['id']).exists())
+        self.assertEquals(Order.objects.get(pk=data['id']).order_transaction.status, "READY")
+
+    def test_사용자가_주문에_대한_결제를_할_수_있다(self):
+        pass
+        # given
+
+        # when
+
+        # then
--- utils/test/fixtures.py
+++ utils/test/fixtures.py
@@ -8,9 +8,27 @@ class TestFixture:
 
     def __init__(self):
         # Member
-        self.non_active_user = User(id=1, username='user1', password='user1user1', is_active=False)
-        self.active_user = User(id=2, username='active_user', password='active_useractive_user', is_active=True)
-        self.other_user = User(id=3, username='other_user', password='other_userother_user', is_active=True)
+        self.non_active_user = User(
+            id=1,
+            username='user1',
+            password='user1user1',
+            email="user1@email.com",
+            is_active=False
+        )
+        self.active_user = User(
+            id=2,
+            username='active_user',
+            password='active_useractive_user',
+            email="active_user@email.com",
+            is_active=True
+        )
+        self.other_user = User(
+            id=3,
+            username='other_user',
+            password='other_userother_user',
+            email="other_user@email.com",
+            is_active=True
+        )
 
         # Credit
         self.credit = Credit(user=self.active_user)
@@ -21,3 +39,14 @@ class TestFixture:
         # Merchandise
         self.merchandise_1 = Merchandise(id=1, username=self.active_user.username, name="상품1", price=10000)
         self.merchandise_2 = Merchandise(id=2, username=self.active_user.username, name="상품2", price=20000)
+        #
+        # # Cart
+        # self.cart = Cart(
+        #     id=1,
+        #     user_id=self.active_user.id,
+        #     merchandise_id=self.merchandise_1.id,
+        #     merchandise_name=self.merchandise_1.name,
+        #     merchandise_price=self.merchandise_1.price,
+        #     merchandise_is_deleted=self.merchandise_1.is_deleted,
+        #     amount=1,
+        # )
