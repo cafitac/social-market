297e9fce09e4312a5b3ef88871288b7d6d8c8411
cafitac <cafitac99@gmail.com>
2024-02-16 22:45:45 +0900

    feat: OrderItem 테이블에 연결되어있는 order foreignkey 필드 이름을 order 로 변경합니다

--- order/models/order_item.py
+++ order/models/order_item.py
@@ -5,7 +5,7 @@ from utils.model.base import AbstractBaseModel
 
 
 class OrderItem(AbstractBaseModel):
-    order_id = models.ForeignKey(to=Order, related_name="order_items", on_delete=models.CASCADE, null=False)
+    order = models.ForeignKey(to=Order, related_name="order_items", on_delete=models.CASCADE, null=False)
     merchandise_id = models.IntegerField(null=False)
     amount = models.IntegerField(null=False)
 
@@ -13,3 +13,7 @@ class OrderItem(AbstractBaseModel):
         db_table = 'order_item'
         verbose_name = 'OrderItem'
         verbose_name_plural = f'{verbose_name} List'
+
+    @staticmethod
+    def create(order: Order, merchandise_id: int, amount: int) -> 'OrderItem':
+        return OrderItem(order=order, merchandise_id=merchandise_id, amount=amount)
