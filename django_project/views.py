232f4f00e111ef73c26845a99fa5149f2ef762f2
cafitac <cafitac99@gmail.com>
2024-02-19 00:07:28 +0900

    feat: 메인화면에서 로그인 사용자의 상품은 보이지 않도록 합니다

--- django_project/views.py
+++ django_project/views.py
@@ -1,4 +1,4 @@
-from django.db.models import QuerySet
+from django.db.models import Q, QuerySet
 from django.shortcuts import render
 
 from merchandise.models import Merchandise
@@ -7,8 +7,12 @@ from merchandise.models import Merchandise
 def index(request):
     q: str = request.GET.get('q')
     merchandises: QuerySet[Merchandise] = Merchandise.objects.filter(is_deleted=False)
+
+    if request.user is not None:
+        merchandises: QuerySet[Merchandise] = merchandises.filter(~Q(username=request.user))
+
     if q is not None:
-        merchandises = merchandises.filter(name__icontains=q)
+        merchandises: QuerySet[Merchandise] = merchandises.filter(name__icontains=q)
 
     return render(request, 'index.html', {
         'merchandises': merchandises,
